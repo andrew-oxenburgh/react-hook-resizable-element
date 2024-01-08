9647bc260280ded1f6c4f3478bac24ab458f8b14
Andrew Oxenburgh <andrew.oxenburgh@gmail.com>
2024-01-08 19:57:29 +1300

    now linting better. indents correct

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'node', 'prettier'],
+  plugins: ['@typescript-eslint', 'node'],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
@@ -12,10 +12,8 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
   ],
   rules: {
-    'prettier/prettier': 'warn',
     'node/no-missing-import': 'off',
     'node/no-empty-function': 'off',
     'node/no-unsupported-features/es-syntax': 'off',
@@ -28,5 +26,9 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/indent": [
+      "error",
+      4,
+    ],
   },
 };
--- test/useResizeElement.spec.ts
+++ test/useResizeElement.spec.ts
@@ -13,13 +13,15 @@ describe('UseResizeElementError', () => {
         const error = UseResizeElementError('test error message');
         expect(error.message).toBe('Error in useResizeElement: test error message');
     });
-})
+});
 
 describe('isNullThrowError', () => {
     it('should throw an error if the value is null or undefined', () => {
         const value = null;
         const message = 'test error message';
-        expect(() => isNullThrowError(value, message)).toThrowError(UseResizeElementError(message));
+        expect(() => isNullThrowError(value, message)).toThrowError(
+            UseResizeElementError(message)
+        );
     });
 
     it('should not throw an error if the value is not null or undefined', () => {
@@ -39,23 +41,31 @@ describe('throwErrorIfElementIsInvalid', () => {
         it('ref.current is null', () => {
             const eleRef = { current: null } as unknown as MutableRefObject<any>;
             callAndExpectThrownException(eleRef);
-        })
+        });
         it('ref.current.scrollHeight is null', () => {
-            const eleRef = { current: { style: { height: '10px' }, scrollHeight: null } } as unknown as MutableRefObject<any>;
+            const eleRef = {
+                current: { style: { height: '10px' }, scrollHeight: null },
+            } as unknown as MutableRefObject<any>;
             callAndExpectThrownException(eleRef);
-        })
+        });
         it('ref.current.style is null', () => {
-            const eleRef = { current: { style: null, scrollHeight: null } } as unknown as MutableRefObject<any>;
+            const eleRef = {
+                current: { style: null, scrollHeight: null },
+            } as unknown as MutableRefObject<any>;
             callAndExpectThrownException(eleRef);
-        })
+        });
         it('ref.current.style.height is null', () => {
-            const eleRef = { current: { style: { height: '10px' }, scrollHeight: null } } as unknown as MutableRefObject<any>;
+            const eleRef = {
+                current: { style: { height: '10px' }, scrollHeight: null },
+            } as unknown as MutableRefObject<any>;
             callAndExpectThrownException(eleRef);
-        })
-    })
+        });
+    });
 
     it('should not throw an error if all of the conditions pass', () => {
-        const eleRef = { current: { style: { height: 'test' }, scrollHeight: 35 } } as unknown as MutableRefObject<ResizableElementShape>;
+        const eleRef = {
+            current: { style: { height: 'test' }, scrollHeight: 35 },
+        } as unknown as MutableRefObject<ResizableElementShape>;
         expect(() => throwErrorIfElementIsInvalid(eleRef)).not.toThrow();
     });
 });
@@ -92,7 +102,7 @@ describe('findDesiredHeight', () => {
         const gapAtBottom = 10;
         const opts = { min, max: 1000, gapAtBottom };
         const desiredHeight = findDesiredHeightInPixels(scrollHeight, opts);
-        expect(desiredHeight).toBe(scrollHeight + gapAtBottom + 'px');
+        expect(desiredHeight).toBe(`${scrollHeight + gapAtBottom}px`);
     });
 });
 
@@ -102,7 +112,7 @@ describe('findDesiredHeightInPixels', () => {
         const min = 50;
         const opts = { min, max: 1000, gapAtBottom: 0 };
         const result = findDesiredHeightInPixels(scrollHeight, opts);
-        expect(result).toBe(scrollHeight + 'px');
+        expect(result).toBe(`${scrollHeight}px`);
     });
 
     it('should return the minimum height if the scrollHeight is less than the minimum height', () => {
@@ -110,7 +120,7 @@ describe('findDesiredHeightInPixels', () => {
         const min = 100;
         const opts = { min, max: 1000, gapAtBottom: 0 };
         const result = findDesiredHeightInPixels(scrollHeight, opts);
-        expect(result).toBe(min + 'px');
+        expect(result).toBe(`${min}px`);
     });
 
     it('should return the maximum height if the scrollHeight is greater than the maximum height', () => {
@@ -119,7 +129,7 @@ describe('findDesiredHeightInPixels', () => {
         const max = 500;
         const opts = { min, max, gapAtBottom: 0 };
         const result = findDesiredHeightInPixels(scrollHeight, opts);
-        expect(result).toBe(max + 'px');
+        expect(result).toBe(`${max}px`);
     });
 
     it('should add the gapAtBottom to the calculated height', () => {
@@ -128,7 +138,7 @@ describe('findDesiredHeightInPixels', () => {
         const gapAtBottom = 10;
         const opts = { min, max: 1000, gapAtBottom };
         const result = findDesiredHeightInPixels(scrollHeight, opts);
-        expect(result).toBe((scrollHeight + gapAtBottom) + 'px');
+        expect(result).toBe(`${scrollHeight + gapAtBottom}px`);
     });
 });
 
@@ -140,7 +150,11 @@ describe('resizeElementPostValidation', () => {
                 height: '10px',
             },
         };
-        resizeElementPostValidation(ele, () => '10px', { min: 0, max: 1000, gapAtBottom: 7 });
+        resizeElementPostValidation(ele, () => '10px', {
+            min: 0,
+            max: 1000,
+            gapAtBottom: 7,
+        });
         expect(ele.style.height).toEqual('10px');
     });
-})
+});
